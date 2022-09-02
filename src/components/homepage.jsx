753d755cde34e6f38f83b582d062e7a79177d431
Success <ikhinobelesuccess@gmail.com>
2022-09-02 23:35:11 +0100

    updates

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -11,6 +11,18 @@ const Homepage = () => {
       <Intro />
       <Details />
       <Whycrappo />
+
+      <div className="bg-[#0D0D2B] text-center text-white font-rubik sm:py-32 px-4 py-20 relative">
+        <h4 className="font-bold sm:text-3xl text-2xl mb-8">
+          Check how much you can learn
+        </h4>
+        <p className="sm:text-sm text-[0.72rem] mb-1">
+          Let's check your hash rate to see how much you will earn today,
+        </p>
+        <p className="sm:text-sm text-[0.72rem] mb-1">
+          Exercitation veniam consequat sunt nostrud amet.
+        </p>
+      </div>
     </div>
   );
 };
